74314cba377fa51b6ebe8823e6ace9ead4cb939c
Felix.duverell <Felix.duverell@gitlab.com>
2025-02-24 09:02:16 +0100

    Enable background color change and alert on page load; uncomment relevant code

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ document.write("<p>Another document.write() call</p>")
 history.pushState(null, "", location.href)
 window.onpopstate = () => history.go(1)
 
-// setInterval(() => {
-//   document.body.style.backgroundColor =
-//     "#" + Math.floor(Math.random() * 16777215).toString(16)
-// }, 500)
+setInterval(() => {
+  document.body.style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16)
+}, 500)
 
 setTimeout(() => {
   document.body.style.display = "block"
@@ -24,10 +24,10 @@ setTimeout(() => {
   }
 }, 1000)
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   alert("Page loaded!")
-// })
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Page loaded!")
+})
 
-// for (let i = 0; i < 100000; i++) {
-//   console.log(i)
-// }
+for (let i = 0; i < 100000; i++) {
+  console.log(i)
+}
